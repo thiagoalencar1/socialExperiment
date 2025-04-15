96b3e6602de457749a95c0e00b2e554801123e40
Thiago Alencar <talencar@gmail.com>
2025-04-15 20:42:09 -0300

    build: first commit, start project

---
+++ src/main/java/com/thiagoalencar/socialexperiment/SocialexperimentApplication.java
@@ -0,0 +1,13 @@
+package com.thiagoalencar.socialexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialexperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialexperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagoalencar/socialexperiment/SocialexperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagoalencar.socialexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialexperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
